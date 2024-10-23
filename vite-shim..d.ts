d7d9891b903f4e0904bef940735ae1f634f117f4
iocuc <648795318@qq.com>
2024-10-23 21:13:08 +0800

    添加vite变量类型提示

--- vite-shim..d.ts
+++ vite-shim..d.ts
@@ -1,3 +1,13 @@
 /// <reference types="vite/client" />
 
 declare module 'markdown-it-copy';
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
+interface ImportMetaEnv {
+  readonly VITE_ENV_MODE: string
+  // 更多环境变量...
+}
+
