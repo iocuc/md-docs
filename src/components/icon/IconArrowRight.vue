6a8596c0a2564e5a1a2097acf5482efb2247e764
iocuc <648795318@qq.com>
2024-10-23 21:43:03 +0800

    添加图标

---
+++ src/components/icon/IconArrowRight.vue
@@ -0,0 +1,9 @@
+<template>
+  <IconSvg>
+    <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"></path>
+  </IconSvg>
+</template>
+
+<script lang="ts" setup>
+import IconSvg from '@/components/icon/IconSvg.vue';
+</script>
